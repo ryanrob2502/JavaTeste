f590b7c8a491c2634ea7242673787b0780d9d785
ryanrob2502 <yukiito2104@gmail.com>
2025-11-14 16:23:24 -0300

    Update EmailService.java

--- src/main/java/com/uniruy/appuser/EmailService.java
+++ src/main/java/com/uniruy/appuser/EmailService.java
@@ -1,8 +1,7 @@
 package com.uniruy.appuser;
 
+import com.sendgrid.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
@@ -10,41 +9,56 @@ import org.springframework.stereotype.Service;
 public class EmailService {
 
     @Autowired
-    private JavaMailSender mailSender;
+    private SendGrid sendGrid;
+
+    private static final String EMAIL_ORIGEM = "contato.medlinksistemamedico@gmail.com";
 
     @Async
     public void enviarCodigoDeLogin(String emailDestino, String codigo) {
-        SimpleMailMessage mensagem = new SimpleMailMessage();
-        mensagem.setFrom("contato.medlinksistemamedico@gmail.com");
-        mensagem.setTo(emailDestino);
-        mensagem.setSubject("MedLink - Sua matricula de Acesso");
-        mensagem.setText("Olá!\n\nObrigado por se registrar no MedLink.\n\nSeu código de acesso é: " + codigo
-                + "\n\nUse este código para fazer login no sistema.\n\nAtenciosamente,\nEquipe MedLink");
+        String assunto = "MedLink - Sua matricula de Acesso";
+        String texto = "Olá!\n\nObrigado por se registrar no MedLink.\n\nSeu código de acesso é: " + codigo
+                + "\n\nUse este código para fazer login no sistema.\n\nAtenciosamente,\nEquipe MedLink";
 
-        mailSender.send(mensagem);
+        enviarEmail(emailDestino, assunto, texto);
     }
 
     @Async
     public void enviarCodigoRedefinicaoSenha(String emailDestino, String codigo) {
-        SimpleMailMessage mensagem = new SimpleMailMessage();
-        mensagem.setFrom("contato.medlinksistemamedico@gmail.com");
-        mensagem.setTo(emailDestino);
-        mensagem.setSubject("MedLink - Código de Verificação");
-        mensagem.setText("Olá!\n\nVocê solicitou a redefinição de senha.\n\nSeu código de verificação é: " + codigo
-                + "\n\nEste código é válido por 30 minutos.\n\nSe você não solicitou esta redefinição, ignore este e-mail.\n\nAtenciosamente,\nEquipe MedLink");
+        String assunto = "MedLink - Código de Verificação";
+        String texto = "Olá!\n\nVocê solicitou a redefinição de senha.\n\nSeu código de verificação é: " + codigo
+                + "\n\nEste código é válido por 30 minutos.\n\nSe você não solicitou esta redefinição, ignore este e-mail."
+                + "\n\nAtenciosamente,\nEquipe MedLink";
 
-        mailSender.send(mensagem);
+        enviarEmail(emailDestino, assunto, texto);
     }
 
     @Async
     public void enviarNovaSenha(String emailDestino, String novaSenha) {
-        SimpleMailMessage mensagem = new SimpleMailMessage();
-        mensagem.setFrom("contato.medlinksistemamedico@gmail.com");
-        mensagem.setTo(emailDestino);
-        mensagem.setSubject("MedLink - Sua Nova Senha Temporária");
-        mensagem.setText("Olá!\n\nVocê solicitou uma nova senha.\n\nSua nova senha temporária é: " + novaSenha
-                + "\n\nPor favor, use-a para fazer login e altere sua senha imediatamente.\n\nAtenciosamente,\nEquipe MedLink");
+        String assunto = "MedLink - Sua Nova Senha Temporária";
+        String texto = "Olá!\n\nVocê solicitou uma nova senha.\n\nSua nova senha temporária é: " + novaSenha
+                + "\n\nPor favor, use-a para fazer login e altere sua senha imediatamente."
+                + "\n\nAtenciosamente,\nEquipe MedLink";
 
-        mailSender.send(mensagem);
+        enviarEmail(emailDestino, assunto, texto);
+    }
+
+    private void enviarEmail(String emailDestino, String assunto, String texto) {
+        try {
+            Email from = new Email(EMAIL_ORIGEM);
+            Email to = new Email(emailDestino);
+            Content content = new Content("text/plain", texto);
+
+            Mail mail = new Mail(from, assunto, to, content);
+
+            Request request = new Request();
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+
+            sendGrid.api(request);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
