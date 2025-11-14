e8f948ddca563c88740a0efa21ef7ec7d68816d0
ryanrob2502 <yukiito2104@gmail.com>
2025-11-14 16:24:47 -0300

    Add files via upload

---
+++ src/main/java/com/uniruy/appuser/SendGridConfig.java
@@ -0,0 +1,14 @@
+package com.uniruy.appuser;
+
+import com.sendgrid.SendGrid;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SendGridConfig {
+
+    @Bean
+    public SendGrid sendGrid() {
+        return new SendGrid(System.getenv("SENDGRID_API_KEY"));
+    }
+}
