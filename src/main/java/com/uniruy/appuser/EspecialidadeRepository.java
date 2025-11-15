7f02dcf39613588ee66623c9e0711cd379497550
ryanrob2502 <yukiito2104@gmail.com>
2025-11-14 22:22:33 -0300

    Delete src directory

--- src/main/java/com/uniruy/appuser/EspecialidadeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.uniruy.appuser;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EspecialidadeRepository extends JpaRepository<Especialidade, Long> {
-
-}
